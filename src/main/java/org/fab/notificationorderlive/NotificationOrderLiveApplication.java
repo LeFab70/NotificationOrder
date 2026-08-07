6755ffa10f681526775f9ddf0852ba3f379a06dc
LeFab70 <kouonang2002@gmail.com>
2026-08-07 01:46:49 -0300

    setup projet

---
+++ src/main/java/org/fab/notificationorderlive/NotificationOrderLiveApplication.java
@@ -0,0 +1,13 @@
+package org.fab.notificationorderlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationOrderLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationOrderLiveApplication.class, args);
+    }
+
+}
